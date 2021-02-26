971ecfca50783985827baee8cfc757cb8f8b0a86
JD <zelenuj@gmail.com>
2021-02-26 20:25:38 +0200

    CU-et5648 Конфіг для категорій. Відключені виробники

---
+++ upload/system/config/d_ajax_search/category_simple.php
@@ -0,0 +1,48 @@
+<?php
+$_['d_ajax_search_category_simple'] = [
+	'table' => [
+		'name' => 'c',
+		'full_name' => 'category',
+		'key' => 'category_id'
+	],
+	'tables' => [
+		[
+			'name' => 'cd',
+			'join_to' => 'c',
+			'full_name' => 'category_description',
+			'key' => 'category_id',
+			'join' => 'LEFT JOIN',
+			'multi_language' => 1
+		],
+		[
+			'name' => 'ct',
+			'join_to' => 'cd',
+			'full_name' => 'category_to_store',
+			'key' => 'category_id',
+			'join' => 'LEFT JOIN',
+			'multi_language' => 1
+		]
+	],
+	'query' => [
+
+		'Name' => [
+			'key' => 'cd.name',
+			'rule' => 'LIKE',
+			'tooltip' => 'Search by Name'
+		],
+
+		'Description' => [
+			'key' => 'cd.description',
+			'rule' => 'LIKE',
+			'tooltip' => 'Search by Description',
+		],
+	],
+
+
+	'select' => [
+		'image' => 'c.image',
+		'name' => 'cd.name',
+		'description' => 'cd.description',
+		'store_id' => 'ct.store_id',
+	]
+];
