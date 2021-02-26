5cc303fefe1d009522ed1bbd87fcdb46cb328c53
JD <zelenuj@gmail.com>
2021-02-26 19:25:09 +0200

    CU-et5648 Переклади ua, ru

---
+++ upload/catalog/language/ru-ru/extension/module/d_ajax_search.php
@@ -0,0 +1,31 @@
+<?php
+
+$_['results_for'] = 'Результаты для';
+$_['search_phase'] ='Введите запрос...';
+$_['no_results'] ='не найдено';
+//what find
+$_['product'] = 'товар';
+$_['category'] = 'категория';
+$_['manufacturer'] = 'производитель';
+$_['information'] = 'информация';
+$_['blog'] = 'блог';
+//find by
+$_['name'] = 'имя';
+$_['description'] = 'опитание';
+$_['model'] = 'модель';
+//$_['manufacturer'] = 'производитель';
+//$_['category'] = 'category';
+$_['attribute'] = 'атрибут';
+$_['title'] = 'заголовок';
+$_['meta_title'] = 'мета-заголовок';
+$_['meta_description'] = 'мета-описание';
+$_['meta_keyword'] = 'ключевые слова';
+$_['more_results'] = 'Показать еще';
+$_['ean'] = 'ean';
+$_['upc'] = 'upc';
+$_['isbn'] = 'isbn';
+$_['mpn'] = 'mpn';
+$_['sku'] = 'sku';
+$_['jan'] = 'jan';
+$_['tag'] = 'tag';
+
---
+++ upload/catalog/language/uk-ua/extension/module/d_ajax_search.php
@@ -0,0 +1,30 @@
+<?php
+
+$_['results_for'] = 'Результати для ';
+$_['search_phase'] ='Введіть пошуковий запит...';
+$_['no_results'] ='нічого не знайдено';
+//what find
+$_['product'] = 'Товар';
+$_['category'] = 'Категорія';
+$_['manufacturer'] = 'Виробник';
+$_['information'] = 'Інформація';
+$_['blog'] = 'блог';
+//find by
+$_['name'] = 'ім\'я';
+$_['description'] = 'опис';
+$_['model'] = 'модель';
+//$_['manufacturer'] = 'виробник';
+//$_['category'] = 'категорія';
+$_['attribute'] = 'атрибут';
+$_['title'] = 'заголовок';
+$_['meta_title'] = 'мета-заголовок';
+$_['meta_description'] = 'мета-опис';
+$_['meta_keyword'] = 'ключові слова';
+$_['more_results'] = 'Показати ще';
+$_['ean'] = 'ean';
+$_['upc'] = 'upc';
+$_['isbn'] = 'isbn';
+$_['mpn'] = 'mpn';
+$_['sku'] = 'sku';
+$_['jan'] = 'jan';
+$_['tag'] = 'tag';
